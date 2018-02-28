82e7845b96ed335f1642c1c3ab0400e910fc0314
Tiszay Ádám <tiszay.adam@gmail.com>
2018-02-28 11:13:52 +0100

    13.fel

--- tech_1.cpp
+++ tech_1.cpp
@@ -1,14 +1,57 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
+
+char k = '#';
+
+void kiir (int db, char c)
+{
+    for(int i=1; i<=db; i++)
+    {
+        cout<<c;
+    }
+}
+
+void kozepre (int hosszu, string szoveg)
+{
+    int db = hosszu-szoveg.length();
+    for(int i=1; i<=db/2; i++)
+        cout<<" ";
+    cout<<szoveg;
+    for(int i=1; i<=db/2; i++)
+        cout<<" ";
+}
+
 
 void greeting(string nev)
 {
-    cout<<"Happy Eastern, "<<nev<<endl;
+    string kiiras;
+    kiir(80,k);
+    cout<<endl;
+    cout<<k;
+    kiir(78,' ');
+    cout<<k;
+    cout<<endl;
+    cout<<k;
+    if(nev=="") kozepre(78, "Happy Eastern, myfriend");
+    else
+    {
+        kiiras = "Happy eastern, " + nev;
+       kozepre(78, kiiras);
+    }
+    cout<<k;
+    cout<<endl;
+    cout<<k;
+    kiir(78,' ');
+    cout<<k;
+    cout<<endl;
+    kiir(80,k);
 }
 
 int main()
 {
-    cout<<"Hello vilag"<<endl;
-
+    string nevnev="myfriend";
+    cin>>nevnev;
+    greeting(nevnev);
     return 0;
 }
